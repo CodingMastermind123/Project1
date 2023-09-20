3d586943a86681c9b9ac76124bd2586dd81401f8
codingmastermindz <145368829+codingmastermindz@users.noreply.github.com>
2023-09-20 10:03:42 -0500

    Add files via upload

---
+++ Background.java
@@ -0,0 +1,19 @@
+import mayflower.*;
+public class Background extends World
+{
+ 
+    /**
+     * Constructor for objects of class MyWorld
+     */
+    public Background()
+    {
+        setBackground("img/background.jpg");
+        
+        showText("WELCOME TO SIDESCROLLER!", 200, 300, Color.WHITE);
+    }
+
+    public void act()
+    {
+       
+    }
+}
