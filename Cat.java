e739f5e6becc416cba0c779a1fb15f15f355a256
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-09-29 10:30:38 -0500

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -3,7 +3,8 @@ import mayflower.*;
 public class Cat extends AnimatedActor {
     private int score;
     private int lives;
-    private MayflowerImage[] frames;
+    private String[] frames;
+    private String[] frames2;
     private int currentFrame;
     private Timer animationTimer;
     private double gravity = 0.5;
@@ -15,47 +16,48 @@ public class Cat extends AnimatedActor {
     private int y;
     private int h;
     private boolean isMoving = false;
+    private Animation walk;
+    private Animation idle;
 
     public Cat() {
         score = 0;
         lives = 3;
-        MayflowerImage p = new MayflowerImage("img/cat/Walk (1).png");
-        p.scale(0.3);
-        setImage(p);
-        frames = new MayflowerImage[10];
+        frames = new String[10];
+        frames2 = new String[10];
         currentFrame = 0;
         animationTimer = new Timer(1000000);
+        for (int i = 1; i < frames.length; i++) {
+                frames[i - 1] = "img/cat/Walk (" + i + ").png";
+            }
+        walk = new Animation(50, frames);   
+        for (int i = 1; i < frames2.length; i++) {
+                frames2[i - 1] = "img/cat/Idle (" + i + ").png";
+            }
+        idle = new Animation(50, frames2);
+        
     }
 
     public void act() {
         
+        super.act();
         
         if(Mayflower.isKeyDown(Keyboard.KEY_RIGHT))
         {
-            for (int i = 1; i < frames.length; i++) {
-                frames[i - 1] = new MayflowerImage("img/cat/Walk (" + i + ").png");
-                frames[i - 1].scale(100, 87);
-            }
+            setAnimation(walk);
         }
         else
         {
-            for (int i = 1; i < frames.length; i++) {
-                frames[i - 1] = new MayflowerImage("img/cat/Idle (" + i + ").png");
-                frames[i - 1].scale(100, 87);
-            }
+            setAnimation(idle);
         }
+        
         
         x = getX();
         y = getY();
         h = getHeight();
-
-        int prevX = x;
-        int prevY = y;
-
-        setImage(frames[currentFrame]);
-        currentFrame = (currentFrame + 1) % frames.length;
         
-
+        // setImage(frames[currentFrame]);
+        // currentFrame = (currentFrame + 1) % frames.length;
+        
         if (Mayflower.isKeyDown(Keyboard.KEY_UP) && !isJumping) {
             velocityY = jumpForce;
             isJumping = true;
