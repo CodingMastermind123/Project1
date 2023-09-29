0caa46b63f3b9918110c93564a6e54a7312cfe8c
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-09-29 10:31:25 -0500

    Update Water.java

--- Water.java
+++ Water.java
@@ -9,6 +9,14 @@ public class Water extends Actor
 
     public void act()
     {
+        if(Mayflower.isKeyDown(Keyboard.KEY_LEFT))
+        {
+            setLocation(getX() + 2, getY());
+        }
+        if(Mayflower.isKeyDown(Keyboard.KEY_RIGHT))
+        {
+            setLocation(getX() - 2, getY());
+        }
         
     }
 }
