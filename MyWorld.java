31c60f092068e286483dee735e654dc8910e5ae8
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-09-29 10:31:02 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,17 +1,18 @@
 import mayflower.*;
 
-
 public class MyWorld extends World 
 {
 
     private Cat cat;
-    
+    private Water water;
     private String[][] tiles;
     public MyWorld() 
     {
         setBackground("img/BG.png");
         cat = new Cat();
+        water = new Water();
         addObject(cat,50, 390);
+        addObject(water,200, 450);
         // dog = new Dog();
         // addObject(dog, 200, 200);
         // jack = new Jack();
@@ -21,12 +22,12 @@ public class MyWorld extends World
         tiles = new String[6][1000];
         buildWorld();
     }
-    
+
     public void act()
     {
-        
+
     }
-    
+
     public void buildWorld()
     {
         for(int i = 0; i < tiles.length; i++)
@@ -46,7 +47,26 @@ public class MyWorld extends World
             {
                 if(tiles[i][j] == "ground")
                 {
-                        addObject(new MovingBlock(), j * 128, i + 472);
+                   addObject(new MovingBlock(), j * 128, i + 472);
+                }
+            }
+        }
+    }
+
+    public void addRandomObjects()
+    {
+        for(int i = 0; i < tiles.length; i++)
+        {
+            for(int j = 0; j < tiles[0].length; j++)
+            {
+                int random = (int)(Math.random() * 8);
+                if(random < 2)
+                {
+                    if(tiles[5][random] == "ground")
+                    {
+                        tiles[5][random] = "water";
+                        addObject(new Water(), j * 100 , i * 100);
+                    }
                 }
             }
         }
