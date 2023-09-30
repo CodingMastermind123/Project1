20ee4674a5af7ddfaa506b695a6975423223c3d7
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-09-30 18:17:06 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,33 +1,38 @@
 import mayflower.*;
 
+
 public class MyWorld extends World 
 {
-
+    private int score;
+    private int lives;
     private Cat cat;
-    private Water water;
+    private Coin coin;
+    
     private String[][] tiles;
     public MyWorld() 
     {
         setBackground("img/BG.png");
+        score = 0;
+        lives = 3;
         cat = new Cat();
-        water = new Water();
-        addObject(cat,50, 390);
-        addObject(water,200, 450);
+        coin = new Coin();
+        addObject(cat,50, 393);
+        addObject(coin, 55, 393);
         // dog = new Dog();
         // addObject(dog, 200, 200);
         // jack = new Jack();
         // addObject(jack, 150, 150);
         // ninja = new Ninja();
         //addObject(ninja, 250, 250);
-        tiles = new String[6][1000];
+        tiles = new String[6][100];
         buildWorld();
     }
-
+    
     public void act()
     {
-
+       updateText(); 
     }
-
+    
     public void buildWorld()
     {
         for(int i = 0; i < tiles.length; i++)
@@ -45,30 +50,32 @@ public class MyWorld extends World
         {
             for(int j = 0; j < tiles[0].length; j++)
             {
-                if(tiles[i][j] == "ground")
+                int random = (int)(Math.random() * 8);
+                Water water = new Water();
+                if(tiles[i][j] == "ground" && random < 3)
                 {
-                   addObject(new MovingBlock(), j * 128, i + 472);
+                    addObject(water, j * 128, i + 472);
+                    tiles[i][j] = "water";
+                }
+                if(tiles[i][j] == "ground")
+                {  
+                    MovingBlock block = new MovingBlock();
+                    block.scale(50, 50);
+                    addObject(block, j * 128, i + 472);
+                }
+                if(j - 1 > 0 && tiles[i][j] == "water" && tiles[i][j - 1] == "water")
+                {
+                    tiles[i][j] = "ground";
+                    removeObject(water);
+                    MovingBlock block = new MovingBlock();
+                    block.scale(50, 50);
+                    addObject(block, j * 128, i + 472);
                 }
             }
         }
     }
-
-    public void addRandomObjects()
-    {
-        for(int i = 0; i < tiles.length; i++)
-        {
-            for(int j = 0; j < tiles[0].length; j++)
-            {
-                int random = (int)(Math.random() * 8);
-                if(random < 2)
-                {
-                    if(tiles[5][random] == "ground")
-                    {
-                        tiles[5][random] = "water";
-                        addObject(new Water(), j * 100 , i * 100);
-                    }
-                }
-            }
-        }
+        private void updateText() {
+        removeText(10, 30);
+        showText("Score: " + score + " Lives: " + lives, 10, 30, Color.BLACK);
     }
 }
