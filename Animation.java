79fa20ffe911952a2a13f7d679c0174fa884775b
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-09-30 18:16:37 -0500

    Update Animation.java

--- Animation.java
+++ Animation.java
@@ -1,32 +1,30 @@
 import mayflower.*;
-public class Animation
-{
+public class Animation {
     private MayflowerImage[] frames;
-    private int framerate;
     private int currentFrame;
-    public Animation(int frameRate, String[] filename)
-    {
-        framerate = frameRate;
+    private int frameDelay;
+    private int frameCounter;
+
+    public Animation(int frameDelay, String[] filename) {
+        this.frameDelay = frameDelay;
         frames = new MayflowerImage[filename.length];
-        for(int i = 0; i < frames.length; i++)
-        {
+        for (int i = 0; i < frames.length; i++) {
             frames[i] = new MayflowerImage(filename[i]);
             frames[i].scale(100, 87);
-            currentFrame = i;
         }
+        currentFrame = 0;
+        frameCounter = 0;
     }
-    
-    public int getFrameRate()
+    public void setFrameDelay(int a)
     {
-        return framerate;
+        frameDelay = a;
     }
-    
-    public MayflowerImage getNextFrame()
-    {
-        currentFrame++;
-        if(currentFrame == 10)
-        {
-            currentFrame = 0;
+    public MayflowerImage getNextFrame() {
+        if (frameCounter >= frameDelay) {
+            currentFrame = (currentFrame + 1) % frames.length;
+            frameCounter = 0;
+        } else {
+            frameCounter++;
         }
         return frames[currentFrame];
     }
