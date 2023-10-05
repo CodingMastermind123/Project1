f7ff96b7adba05020b678a4dd2aebf8da4dc3244
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-10-04 23:10:35 -0500

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -17,12 +17,16 @@ public class Cat extends AnimatedActor {
     private Animation idle;
     private int score;
     private int lives;
-    boolean invulnerable = false;
-    Timer invulnerableTimer = new Timer(100000);
+    private boolean isInvincible;
+    private int invincibilityCooldown;
+
+    private boolean lifeLost = false;
 
     public Cat() {
         score = 0;
         lives = 3;
+        isInvincible = false;
+        invincibilityCooldown = 0;
         frame = new String[10];
         frame2 = new String[10];
         for (int i = 1; i <= frame.length; i++) {
@@ -70,20 +74,61 @@ public class Cat extends AnimatedActor {
         }
 
         setLocation(x, y);
+        checkCoinCollision();
+        checkWaterCollision();
         updateText();
 
-        if(isTouching(Water.class) && invulnerable == false)
-        {
-            decreaseLives();
-            setInvulnerable();
-
-            if(lives == 0)
-            {
-                World w = getWorld();
-                w.removeText(10, 30);
-                w.showText("Game Over", 400, 300, Color.BLACK);
+        if (isInvincible) {
+            invincibilityCooldown--;
+            if (invincibilityCooldown <= 0) {
+                isInvincible = false;
+                makeVisible();
             }
         }
+    }
+
+    private void updateText() {
+        World w = getWorld();
+        w.removeText(10, 30);
+        w.showText("Score: " + score + " Lives: " + lives, 10, 30, Color.BLACK);
+    }
+
+    private void checkCoinCollision() {
+        Coin coin = getOneIntersectingObject(Coin.class);
+        System.out.println("checking touching coin...");
+
+        if (coin != null) {
+            getWorld().removeObject(coin);
+            score++;
+            updateText();
+        }
+    }
+
+    public boolean checkWaterCollision() {
+    Water water = getOneIntersectingObject(Water.class);
+
+    if (water != null && !isInvincible) {
+        lives--;
+        updateText(); 
+        isInvincible = true;
+        invincibilityCooldown = 100; 
+        makeInvisible(); 
+        resetPosition();
+        return true;
+    }
+    return false;
+}
+
+    private void makeInvisible() {
+        getImage().setTransparency(0);  // Set transparency to 0 to hide the cat
+    }
+
+    private void makeVisible() {
+        getImage().setTransparency(255);  // Set transparency to 255 to make the cat visible
+    }
+
+    public void resetPosition() {
+        setLocation(50, groundY); 
     }
 
     public int getScore()
@@ -96,36 +141,15 @@ public class Cat extends AnimatedActor {
         return lives;
     }
 
-    public void decreaseLives()
+    public void loseLife()
     {
-        lives = lives - 1;
+        lives--;
+        lifeLost = true;
         updateText();
     }
-    
-    public void increaseScore()
-    {
-        score++;
-    }
-    public void updateText()
-    {
-        World w = getWorld();
-        w.removeText(10, 30);
-        w.showText("Score: " + score + " Lives: " + lives, 10, 30, Color.BLACK);
-    }
 
-    public void setInvulnerable()
+    public void makeFalse()
     {
-        invulnerable = true;
-        invulnerableTimer.reset();
-    }
-
-    public boolean isInvulnerable()
-    {
-        if(invulnerableTimer.isDone())
-        {
-            invulnerable = false;
-        }
-
-        return invulnerable;
+        lifeLost = false;
     }
 }
